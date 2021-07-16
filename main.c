9d76415ff229a2a0c381e4a500d4a71405f4fb0e
Igor Vendramini <contagame011@gmail.com>
2021-07-16 17:41:27 -0300

    arquivo mega importante

---
+++ main.c
@@ -0,0 +1,16 @@
+
+int main(){
+
+    int x;
+
+    printf("Digite um numero maior que 0: ");
+    scanf("%i",&x);
+    while(x<=0){
+        printf("Eu disse maior que 0 bb digita de novo: ");
+        scanf("%i",&x);
+    }
+
+    printf("\nPosha so %i, na vdd, eu sei que vc me ama mais que isso <3",x);
+
+    return 0;
+}
